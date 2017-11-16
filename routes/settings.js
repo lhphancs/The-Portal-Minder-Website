4b93fec9dd81907b9173f2ee9d6203f59a7aeb7a
Lee Phan <lhphan@personalmicrosoftsoftware.uci.edu>
2017-11-15 17:05:23 -0800

    save setting working

--- routes/settings.js
+++ routes/settings.js
@@ -6,13 +6,24 @@ var UserModel = require('../models/UserModel');
 router.use(auth.require_login);
 
 router.get('/', function(req, res, next) {
-  res.render('settings', { title: 'Minder Settings' });
+  UserModel.findOne( { _id: req.user._id }, function(err, user){
+    res.render('settings', { settings: user.settings, title: 'Minder Settings' });
+  } );
 });
 
+
+
 router.post('/save', function(req, res, next){
-  console.log(req.body);
-  console.log(typeof(req.body.accepted_friend_requests));
-  res.send(true);
+  var self_id = req.user._id;
+  is_setting_friend_accept = req.body.accepted_friend_requests === 'on';
+  is_setting_chat_initiated = req.body.chat_initiated === 'on';
+  UserModel.update( {_id: self_id}, {
+    settings: {notification: {friendAccepted: is_setting_friend_accept,
+                chatInitiated: is_setting_chat_initiated} }
+  }, function(err, data){
+    ;
+  } );
+  res.redirect("/settings");
 });
 
 module.exports = router;
