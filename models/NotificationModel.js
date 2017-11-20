0b66375ac20b5723a48c005e5cea5f81a78d0c90
Lee Phan <lhphan@personalmicrosoftsoftware.uci.edu>
2017-11-20 11:44:23 -0800

    cleaned schema a bit

--- models/NotificationModel.js
+++ models/NotificationModel.js
@@ -3,8 +3,8 @@ var Schema = mongoose.Schema,
 ObjectId = Schema.ObjectId;
 
 var notificationsSchema = mongoose.Schema({
-  userId: {type: ObjectId},
-    fromId: {type: ObjectId},
+  userId: ObjectId,
+    fromId: ObjectId,
     message: String,
     timestamp : {type: Date, default: Date.now},
     isRead: {type: Boolean, default: false} }
